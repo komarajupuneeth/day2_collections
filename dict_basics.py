e078688c8918c9b9b08a5d8e3150b4d301cd10cf
komarajupuneeth <komarajupuneeth@gmail.com>
2025-11-27 12:55:10 +0530

    Add files via upload

--- dict_basics.py
+++ dict_basics.py
@@ -10,3 +10,18 @@ print(student)
 #accesing values 
 print("name: ",student["name"]) 
 print("age: ",student["age"])
+
+#adding a new keyvalue pair 
+student["college"]="asdb college"
+print("after adding college: ",student)
+
+#updating existing key-value pair 
+student["marks"]=99
+print("after updating marks: ",student)
+
+#deleting a key_value pair 
+del student["branch"]
+print("after deleting branch: ",student)
+
+#checking if a key exists 
+print("Has 'age'? : ","age" in student)
